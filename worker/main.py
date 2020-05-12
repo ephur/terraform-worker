9f8a616d073185c625ac0b122f3ea323840bc477
ephur <richard.maynard@gmail.com>
2020-05-12 08:27:06 -0500

    aiml changes

--- worker/main.py
+++ worker/main.py
@@ -46,87 +46,95 @@ class State(object):
         return None
 
     def load_config(self, config_file):
-        with open(config_file, 'r') as cfile:
+        with open(config_file, "r") as cfile:
             self.config = ordered_config_load(cfile, self.args)
 
     class StateArgs(object):
         """A class to hold arguments in the state for easier access."""
+
         pass
 
 
 def generate_keypair(tempdir, name):
     """Generate an ssh keypair, and write to the tempdir."""
     key = rsa.generate_private_key(
-        backend=crypto_default_backend(),
-        public_exponent=65537,
-        key_size=4096
+        backend=crypto_default_backend(), public_exponent=65537, key_size=4096
     )
 
     private_key = key.private_bytes(
         crypto_serialization.Encoding.PEM,
         crypto_serialization.PrivateFormat.PKCS8,
-        crypto_serialization.NoEncryption()).decode()
-
-    public_key = key.public_key().public_bytes(
-        crypto_serialization.Encoding.OpenSSH,
-        crypto_serialization.PublicFormat.OpenSSH
+        crypto_serialization.NoEncryption(),
     ).decode()
 
-    with open("{}/{}".format(tempdir, name), mode='w') as priv_key:
+    public_key = (
+        key.public_key()
+        .public_bytes(
+            crypto_serialization.Encoding.OpenSSH,
+            crypto_serialization.PublicFormat.OpenSSH,
+        )
+        .decode()
+    )
+
+    with open("{}/{}".format(tempdir, name), mode="w") as priv_key:
         priv_key.write(private_key)
         os.chmod("{}/{}".format(tempdir, name), 0o600)
 
-    with open("{}/{}.pub".format(tempdir, name), mode='w') as pub_key:
+    with open("{}/{}.pub".format(tempdir, name), mode="w") as pub_key:
         pub_key.write(public_key)
 
-    return("{}/{}.pub".format(tempdir, name), "{}/{}".format(tempdir, name))
+    return ("{}/{}.pub".format(tempdir, name), "{}/{}".format(tempdir, name))
 
 
 def create_table(name, region, key_id, key_secret, read_capacity=1, write_capacity=1):
     """Create a dynamodb table."""
-    client = boto3.client('dynamodb', region_name=region, aws_access_key_id=key_id, aws_secret_access_key=key_secret)
+    client = boto3.client(
+        "dynamodb",
+        region_name=region,
+        aws_access_key_id=key_id,
+        aws_secret_access_key=key_secret,
+    )
     tables = client.list_tables()
-    table_key = 'LockID'
-    if name in tables['TableNames']:
-        click.secho("DynamoDB lock table found, continuing.", fg='yellow')
+    table_key = "LockID"
+    if name in tables["TableNames"]:
+        click.secho("DynamoDB lock table found, continuing.", fg="yellow")
     else:
-        click.secho("DynamoDB lock table not found, creating, please wait...", fg='yellow')
+        click.secho(
+            "DynamoDB lock table not found, creating, please wait...", fg="yellow"
+        )
         client.create_table(
             TableName=name,
-            KeySchema=[
-                {
-                    'AttributeName': table_key,
-                    'KeyType': 'HASH'
-                }
-            ],
-            AttributeDefinitions=[
-                {
-                    'AttributeName': table_key,
-                    'AttributeType': 'S'
-                },
-            ],
+            KeySchema=[{"AttributeName": table_key, "KeyType": "HASH"}],
+            AttributeDefinitions=[{"AttributeName": table_key, "AttributeType": "S"},],
             ProvisionedThroughput={
-                'ReadCapacityUnits': read_capacity,
-                'WriteCapacityUnits': write_capacity
-            }
+                "ReadCapacityUnits": read_capacity,
+                "WriteCapacityUnits": write_capacity,
+            },
         )
 
-        client.get_waiter('table_exists').wait(TableName=name, WaiterConfig={'Delay': 10, 'MaxAttempts': 30})
+        client.get_waiter("table_exists").wait(
+            TableName=name, WaiterConfig={"Delay": 10, "MaxAttempts": 30}
+        )
 
 
 def get_aws_id(key_id, key_secret):
     """Return the AWS account ID."""
-    client = boto3.client('sts', aws_access_key_id=key_id, aws_secret_access_key=key_secret)
-    return client.get_caller_identity()['Account']
+    client = boto3.client(
+        "sts", aws_access_key_id=key_id, aws_secret_access_key=key_secret
+    )
+    return client.get_caller_identity()["Account"]
 
 
-def ordered_config_load(stream, args, Loader=yaml.SafeLoader, object_pairs_hook=OrderedDict):
+def ordered_config_load(
+    stream, args, Loader=yaml.SafeLoader, object_pairs_hook=OrderedDict
+):
     """
     Load a yaml config, and replace templated items.
 
     Derived from:
     https://stackoverflow.com/questions/5121931/in-python-how-can-you-load-yaml-mappings-as-ordereddicts
     """
+
     class OrderedLoader(Loader):
         pass
 
@@ -139,9 +147,10 @@ def ordered_config_load(stream, args, Loader=yaml.SafeLoader, object_pairs_hook=
 
         loader.flatten_mapping(node)
         return object_pairs_hook(loader.construct_pairs(node))
+
     OrderedLoader.add_constructor(
-        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-        construct_mapping)
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, construct_mapping
+    )
     return yaml.load(stream, OrderedLoader)
 
 
@@ -154,7 +163,7 @@ def replace_vars(var, args):
     try:
         var = getattr(args, match.group(1).replace("-", "_"))
     except AttributeError:
-        raise(ValueError("substitution not found for {}".format(var)))
+        raise (ValueError("substitution not found for {}".format(var)))
     return var
 
 
@@ -175,15 +184,37 @@ def pipe_exec(args, stdin=None, cwd=None, env=None):
     for i in args:
         if count == 0:
             if stdin is None:
-                commands.append(subprocess.Popen(shlex.split(i), stdout=subprocess.PIPE,
-                                                 stderr=subprocess.PIPE, cwd=cwd, env=env))
+                commands.append(
+                    subprocess.Popen(
+                        shlex.split(i),
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                        cwd=cwd,
+                        env=env,
+                    )
+                )
             else:
-                commands.append(subprocess.Popen(shlex.split(i), stdout=subprocess.PIPE,
-                                                 stderr=subprocess.PIPE, stdin=subprocess.PIPE, cwd=cwd, env=env))
+                commands.append(
+                    subprocess.Popen(
+                        shlex.split(i),
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                        stdin=subprocess.PIPE,
+                        cwd=cwd,
+                        env=env,
+                    )
+                )
         else:
-            commands.append(subprocess.Popen(shlex.split(i), stdout=subprocess.PIPE,
-                                             stderr=subprocess.PIPE, stdin=commands[count - 1].stdout, cwd=cwd,
-                                             env=env))
+            commands.append(
+                subprocess.Popen(
+                    shlex.split(i),
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    stdin=commands[count - 1].stdout,
+                    cwd=cwd,
+                    env=env,
+                )
+            )
         count = count + 1
 
     if stdin is not None:
