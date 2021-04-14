8adaf21b04001df12a62274e73267c3772daceb3
ephur <richard.maynard@gmail.com>
2021-04-14 13:50:10 -0500

    remove unused import

--- tests/backends/test_gcs.py
+++ tests/backends/test_gcs.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from unittest import mock
-from unittest.mock import call, patch
+from unittest.mock import patch
 
 import pytest
 from tfworker.backends.base import BackendError
